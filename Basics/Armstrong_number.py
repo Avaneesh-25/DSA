81712d2c313fbd5bfd9651bcf5c8af1f4d245a82
Avaneesh <avaneeshvarma25@gmail.com>
2026-07-08 16:05:34 +0530

    basics

--- Basics/Armstrong_number.py
+++ Basics/Armstrong_number.py
@@ -1,10 +1,11 @@
 n = int(input())
 original = n
 sum = 0
+digits = len(str(n))
 while n > 0:
     last_digit = n % 10
     n = n // 10
-    sum = sum + last_digit ** 3
+    sum = sum + last_digit ** digits
 print(sum, end="")
 if sum == original:
     print("Armstrong number")
