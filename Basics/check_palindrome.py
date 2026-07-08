1ee7c6d3c621c2f21f65d3e832d3f771ac7ec086
Avaneesh <avaneeshvarma25@gmail.com>
2026-07-08 15:49:15 +0530

    basics and leet code solution in comment lines

--- Basics/check_palindrome.py
+++ Basics/check_palindrome.py
@@ -11,3 +11,21 @@ if reverse_num == dup:
     print("palindrome")
 else:
     print("not palindrome")
+
+#leetcode problem
+'''
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        original = x
+        reverse_num = 0
+
+        while x > 0:
+            last_digit = x % 10
+            x = x // 10
+            reverse_num = reverse_num * 10 + last_digit
+
+        return reverse_num == original
+'''        
