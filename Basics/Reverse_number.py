7207984249e28c8917e14b3293b7ff882649c51d
Avaneesh <avaneeshvarma25@gmail.com>
2026-07-08 15:08:24 +0530

    basics

--- Basics/Reverse_number.py
+++ Basics/Reverse_number.py
@@ -1,17 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        reverse_number = 0
-        negative= x < 0
+        negative = x < 0
         x = abs(x)
+
+        reverse_number = 0
 
         while x > 0:
             last_digit = x % 10
             x = x // 10
-            reverse_number = reverse_number * 10 + (last_digit)
-        
+            reverse_number = reverse_number * 10 + last_digit
+
         if negative:
-            reverse_number =  -reverse_number
+            reverse_number = -reverse_number
+
+        if reverse_number < -2**31 or reverse_number > 2**31 - 1:
+            return 0
 
         return reverse_number
-
-
